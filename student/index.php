7b91b114fe932453a1c6d78ea22edfb483a8ff75
John Paragas <63882260+johndwight@users.noreply.github.com>
2022-10-10 15:52:11 -0400

    Re-structured student pages with Joel's fixes

--- student/index.php
+++ student/index.php
@@ -20,10 +20,11 @@
 			}
 		</style>
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
-		<link rel="stylesheet" type="text/css" href="./css/bootstrap.min.css" />
+		<link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css" />
 	</head>
 
 	<body>
+		<?php require_once("navbar.php"); ?>
 		<?php
 		// Checking user cookie
 		if (!isset($_COOKIE["personID"])) {
@@ -47,7 +48,7 @@
 		print("<div class=\"col-sm-2\">");
 		print("</div>"); // End First Col
 		print("<div class=\"col-sm-8\">");
-		print("<h2>Your courses</h2>");
+		print("<h2 style='padding-top: 10px; padding-bottom: 10px'>Your courses</h2>");
 		print("</div>"); // End Second Col
 		print("<div class=\"col-sm-2\">");
 		print("</div>"); // End Third Col
@@ -62,8 +63,9 @@
 			ON Courses.courseID = Registrations.courseID
 			INNER JOIN Student
 			ON Registrations.studentID = Student.studentID
-			WHERE Student.PersonID = {$_COOKIE["personID"]}
+			WHERE Student.StudentID = {$_COOKIE["personID"]}
 			ORDER BY courseCode ASC";
+
 
 		// Query registered courses
 		if (!($result = mysqli_query($database, $selectDroppable))) {
@@ -133,7 +135,6 @@
 		}
 		mysqli_close($database);
 		?>
-		<h2><a href="manageCourses.php">Manage Courses</a></h2>
 		<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 		<script src="./js/bootstrap.bundle.min.js"></script>
 	</body>
--- student/navbar.php
+++ student/navbar.php
@@ -3,7 +3,7 @@
         <div class="collapse navbar-collapse" id="navbarNavDropdown">
             <ul class="navbar-nav">
                 <li class="nav-item" style="margin-left: 40px">
-                    <a class="nav-link" href="index.php">Registered Courses</a>
+                    <a class="nav-link" href="index.php">Home</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="manageCourses.php">Manage Courses</a>
