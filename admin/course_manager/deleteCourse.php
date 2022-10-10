f59f26f62a0e1c5cef3cdc08eb5988b99cc9af84
John Paragas <63882260+johndwight@users.noreply.github.com>
2022-10-09 23:52:18 -0400

    Implement delete courses functionality
    Re-used code from manageCourses.php

--- admin/course_manager/deleteCourse.php
+++ admin/course_manager/deleteCourse.php
@@ -1,4 +1,5 @@
 <!DOCTYPE html">
+<html xmlns="http://www.w3.org/1999/xhtml">
 <?php
 // Setting user cookie.
 $cookie_name = "admin_user";
@@ -7,53 +8,156 @@ setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/"); // 86400 = 1
 ?>
 <html>
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <title>Add a Course</title>
+
+<head class="page-header header container-fluid-fluid">
+    <title>Delete a Course</title>
     <style type="text/css">
-        body {
-            background-color: #F0E68C
+        table {
+            background-color: #ADD8E6
         }
 
-        h2 {
-            font-family: arial, sans-serif;
-            color: blue
-        }
-
-        input {
-            background-color: blue;
-            color: yellow;
-            font-weight: bold
-        }
-
-        form {
-            width: 50%;
-            margin: 10px auto;
-            padding: 20px;
-            border-width: 2px;
-            border-color: black;
-            border-style: solid;
-            border-radius: 10px;
+        td {
+            padding-top: 2px;
+            padding-bottom: 2px;
+            padding-left: 4px;
+            padding-right: 4px;
+            border-width: 1px;
+            border-style: inset
         }
     </style>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" type="text/css" href="../../css/bootstrap.min.css" />
 </head>
 
 <body>
-    <form action="">
-        <div class="form-group">
-            <label for="courseCodeInput">Course Code</label>
-            <input type="text" class="form-control" id="courseCodeInput" placeholder="e.g. SOEN387">
+    <div class="container-fluid">
+        <div class="jumbotron text-center title">
+            <h1>Delete courses</h1>
         </div>
-        <button type="submit" class="btn btn-primary">Delete</button>
-    </form>
+    </div>
+    <?php require_once("../../database/setup.php");
+    if (isset($_POST['action'])) {
+        if ($_POST['action'] == 'Delete') {
+            // We execute an add query before displaying the page.
+            $courseId = $_POST['courseId'];
+            $deleteCourse = "DELETE FROM Courses WHERE courseID = $courseId";
 
+            if (!($addResult = mysqli_query($conn, $deleteCourse))) {
+                print("<div class=\"container-fluid\">");
+                print("<div class=\"row\">");
+                print("<div class=\"col-sm-4\">");
+                print("</div>"); // End First Col
+                print("<div class=\"col-sm-4 justify-content-center\">");
+                print("<div class=\"alert alert-danger text-center\">");
+                print("<p>Could not delete the course! Please try again later.</p>");
+                print(mysqli_error($conn));
+                print("</div>");
+                print("</div>"); // End Second Col
+                print("<div class=\"col-sm-4\">");
+                print("</div>"); // End Third Col
+                print("</div>"); // End Row
+                print("</div>"); // End container-fluid
+            } else {
+                print("<div class=\"container-fluid\">");
+                print("<div class=\"row\">");
+                print("<div class=\"col-sm-4\">");
+                print("</div>"); // End First Col
+                print("<div class=\"col-sm-4 justify-content-center\">");
+                print("<div class=\"alert alert-success text-center\">");
+                print("<p>Deleted course successfully!</p>");
+                print(mysqli_error($conn));
+                print("</div>");
+                print("</div>"); // End Second Col
+                print("<div class=\"col-sm-4\">");
+                print("</div>"); // End Third Col
+                print("</div>"); // End Row
+                print("</div>"); // End container-fluid
+            }
+        }
+    }
 
-    <script src=" https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    $selectDeletable = " SELECT Courses.CourseId, Courses.courseCode, Courses.title, 
+ Courses.semester, courses.instructor, courses.startDate, courses.endDate 
+ FROM Courses
+ ORDER BY courseCode ASC";
+    // Query registered courses
+    if (!($result = mysqli_query($conn, $selectDeletable))) {
+        print("<div class=\"container-fluid\">");
+        print("<div class=\"row\">");
+        print("<div class=\"col-sm-4\">");
+        print("</div>"); // End First Col
+        print("<div class=\"col-sm-4 justify-content-center\">");
+        print("<div class=\"alert alert-danger text-center\">");
+        print("<p>Could not execute query to retrieve registered courses!</p>");
+        print(mysqli_error($conn));
+        print("</div>");
+        print("</div>"); // End Second Col
+        print("<div class=\"col-sm-4\">");
+        print("</div>"); // End Third Col
+        print("</div>"); // End Row
+        print("</div>"); // End container-fluid
+    } else {
+        if (mysqli_num_rows($result) > 0) {
+            print("<div class=\"container-fluid\">");
+            print("<div class=\"row justify-content-center\">");
+            print("<div class=\"col-sm-2\">");
+            print("</div>"); // End First Col
+            print("<div class=\"col-sm-8\">");
+            print("<table class=\"table table-responsive table-striped table-bordered table-hover\"> 
+                                <tr> 
+                                    <th>Course Code</th>
+                                    <th>Title</th>
+                                    <th>Semester</th>
+                                    <th>Instructor</th>
+                                    <th>Start Date</th>
+                                    <th>End Date</th>
+                                    <th></th>
+                                </tr>");
+
+            // Loop over the rows returned, showing them in a table.
+            while ($row = $result->fetch_assoc()) {
+                print("<tr>");
+                print("<td>{$row['courseCode']}</td>");
+                print("<td>{$row['title']}</td>");
+                print("<td>{$row['semester']}</td>");
+                print("<td>{$row['instructor']}</td>");
+                print("<td>{$row['startDate']}</td>");
+                print("<td>{$row['endDate']}</td>");
+                // Clickable button to drop a course
+                print("<td>
+                            <form class=\"justify-content-center text-center\" method=\"post\" action=\"\">
+                                <input class=\"btn btn-primary\" type=\"submit\" name=\"action\" value=\"Delete\"/>
+                                <input type=\"hidden\" name=\"courseId\" value=\"{$row['CourseId']}\"/>
+                            </form>
+                            </td>");
+                print("</tr>");
+            }
+            print("</table>");
+            print("</div>"); // End Second Col
+            print("<div class=\"col-sm-2\">");
+            print("</div>"); // End Third Col
+            print("</div>"); // End Row
+            print("</div>"); // End container-fluid
+        } else {
+            print("<div class=\"container-fluid\">");
+            print("<div class=\"row\">");
+            print("<div class=\"col-sm-4\">");
+            print("</div>"); // End First Col
+            print("<div class=\"col-sm-4\">");
+            print("<h3>You are not registered to any courses at this time.</h3>");
+            print(mysqli_error($conn));
+            print("<br />");
+            print("</div>"); // End Second Col
+            print("<div class=\"col-sm-4\">");
+            print("</div>"); // End Third Col
+            print("</div>"); // End Row
+            print("</div>"); // End container-fluid
+        }
+    }
+    ?>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+    <script src="../../js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
