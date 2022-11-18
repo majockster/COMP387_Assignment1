dca418d6a00da1ee49f67a164a178383b9ea93ec
Hamzah Muhammad <99919629+hamzahmhd@users.noreply.github.com>
2022-11-18 01:23:08 -0500

    Update logout.jsp

--- A2/387A2/src/main/webapp/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-    // Clearing cookie
-    setcookie('firstName', null, -1, "/");
-    setcookie('lastName', null, -1, "/");
-    setcookie('personID', null, -1, "/");
-    setcookie('userType', null, -1, "/");
-    header('Location:./');
-?>
